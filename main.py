b8a03a0ae2bfb3f07f0b04189a4c136420fbbabc
JonatasRG <70361185+JonatasRG@users.noreply.github.com>
2021-01-08 14:33:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,5 @@ if __name__ == '__main__':
     teste.gera_lista_de_primos(40)
 
     print(teste.verifica_se_e_primo(311))
+    print(teste.verifica_se_e_primo(391))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
